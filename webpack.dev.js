1cac0c0d5e3354fa372255733ce216cdc727ff55
RuduBear <134881527+rudubear@users.noreply.github.com>
2026-03-16 19:38:36 -0400

    splitting webpack into prod and dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,38 +1,13 @@
 // webpack.config.js
 import path from "node:path";
+import common from "./webpack.common.js";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import { merge } from 'webpack-merge';
 
-export default {
+export default merge (common, {
     mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-};
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,38 +1,9 @@
 // webpack.config.js
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import common from "./webpack.common.js";
+import { merge } from 'webpack-merge';
 
-export default {
+export default merge (common, {
     mode: "production",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-};
+});
